f0a0af134be1c56247d211afc870fb48db0fc6ec
Tan-Thai <Vi.Tan.Thai@gmail.com>
2024-09-02 01:13:18 +0200

    Shortened scanner file again
    Used a Do/While loop and skipped the creation of a method

--- Coding_playground/Scanner/TestScanner.java
+++ Coding_playground/Scanner/TestScanner.java
@@ -6,26 +6,37 @@ public class TestScanner {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please input a number");
+        System.out.println("Please input a number between 18 to 60");
         int number = 0;
 
-        while (number < 18 || number > 60) {
-            number = numberCheck(input);
-            System.out.println("the number you input is not valid");
-
+        do {
+            while (!input.hasNextInt()) {
+                System.out.println("invalid input, please write a number");
+                input.next();
+            }
+            System.out.println("The number you gave is not valid");
+            number = input.nextInt();
         }
+        while (number < 18 || number > 60);
         System.out.println("The current input number is " + number);
+
+        // while (number < 18 || number > 60) {
+        //     number = numberCheck(input);
+        //     System.out.println("the number you input is not valid");
+
+        // }
+        // System.out.println("The current input number is " + number);
         
         input.close();
     }
 
-    private static int numberCheck(Scanner input){
-        while (!input.hasNextInt()){
-            input.nextLine();
-            System.out.println("invalid input, please write a number");
-        }
-        return input.nextInt();
-    }
+    // private static int numberCheck(Scanner input){
+    //     while (!input.hasNextInt()){
+    //         input.next();
+    //         System.out.println("invalid input, please write a number");
+    //     }
+    //     return input.nextInt();
+    // }
 
 }
 
