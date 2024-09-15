f975d5c7b7143aff2216a3aea68e0929efeb258b
Tan-Thai <Vi.Tan.Thai@gmail.com>
2024-09-16 01:54:27 +0200

    Starting on case 3 in challange 2
    Lots of adjustments for the loop. singular odd enter needed for case 2

--- Coding_playground/Challange_2.java
+++ Coding_playground/Challange_2.java
@@ -1,90 +1,128 @@
 package Coding_playground;
+
 import java.util.Scanner;
 import java.text.MessageFormat;
 
 public class Challange_2 {
-    
+
     private Scanner scanner = new Scanner(System.in);
+
     public void main(String[] args) {
         Scanner sc = getScanner();
-        
-        System.out.println("Welcome to this practice program!\nYou will get to choose your actions, please input what you wish to do.");
-        
-        System.out.println("What do you wish to do?");
-        System.out.println("1.Add 2 numbers.\n2.Count letters in a string. \n3.Mirror a string.\n4.The sum of all numbers in a string.");
-        System.out.println("Please write a number between 1-4");
+        char chosenOption;
 
-        int tempChoiceNum = 0; 
-        
+        System.out.println(
+                "Welcome to this practice program!\nYou will get to choose your actions, please input what you wish to do.");
         do {
-            tempChoiceNum = checkIfNumber(sc);
-        } while (tempChoiceNum > 4 || tempChoiceNum == 0);
+            System.out.println("What do you wish to do?");
+            System.out.println(
+                    "1.Add 2 numbers.\n2.Count letters in a string. \n3.Mirror a string.\n4.The sum of all numbers in a string.");
+            System.out.println("Please write a number between 1-4 or e to exit. --");
 
-        sc.nextLine();
-        
-        System.out.println("The number you chose is " + tempChoiceNum);
-        
-        switch (tempChoiceNum) {
-            case 1:
-                System.out.println("You chose option 1");
-                addTwoNumbers();
-            break;
-            
-            case 2:
-                System.out.println("You chose option 2");
+            chosenOption = optionChecker(sc);
 
-                break;
-            
-            // case 3:
-            // System.out.println("You chose option 3");
-            
-            // break;
-            
-            // case 4:
-            // System.out.println("You chose option 4");
-            
-            // break;
+            if (chosenOption == 'e') {
+                System.out.println("Exiting program, good bye.");
+                return;
             }
 
-        }
-        
-        public Scanner getScanner(){
-            return scanner;
-        }
-        
-        public void addTwoNumbers(){
-            Scanner sc = getScanner();
-            System.out.println("Please input the two numbers you want to add togehter, starting with the first:");
-            int firstNumber = 0;
-            int secondNumber = 0;
+            System.out.println("-----------\nYou chose option " + chosenOption);
+            switch (chosenOption) {
+                case '1':
+                    caseOne();
+                    break;
 
-            firstNumber = checkIfNumber(sc);
-            System.out.println("the first number is " + firstNumber);
-            secondNumber = checkIfNumber(sc);
-            System.out.println("the first number is " + secondNumber);
+                case '2':
+                    caseTwo();
+                    break;
 
-            System.out.println("The total of the 2 given numbers is " + (firstNumber + secondNumber));
-        }
+                case '3':
+                    caseThree();
+                    break;
+                // case 4:
+                // System.out.println("You chose option 4");
 
-        // https://stackoverflow.com/questions/71872221/java-how-to-use-a-scanner-to-check-that-input-is-an-integer-and-is-within-a-spe
-
-
-
-        public int checkIfNumber(Scanner sc){
-            while (!sc.hasNextInt()) {
-                System.out.println("Invalid input, please write a number");
-                sc.nextLine();
+                // break;
             }
-            return sc.nextInt();
-        }
+            sc.nextLine();
+            System.out.println("--------Returning to the main menu--------");
 
-        public int inputAction(){
-            Scanner sc = getScanner();
+        } while (chosenOption != 'e');
+    }
 
-            int n = 0;
-            while (n < 1 || n > 5) {
-                n = checkIfNumber(sc);
+    public char optionChecker(Scanner sc) {
+        do {
+            String inputString = sc.nextLine().trim();
+            if (inputString.length() == 1) {
+                char choice = inputString.charAt(0);
+
+                if ((choice >= '1' && choice <= '4') || choice == 'e') {
+                    return choice;
+                }
             }
-            return n; 
+            System.out.println("Invalid input, please enter a number between 1-4 or e to exit.");
+        } while (true); // tvingar en loop tills användaren har skrivit rätt.
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void caseOne() {
+        Scanner sc = getScanner();
+        System.out.println("Please input the two numbers you want to add togehter, starting with the first:");
+        int firstNumber = 0;
+        int secondNumber = 0;
+
+        firstNumber = checkIfNumber(sc);
+        System.out.println("the first number is " + firstNumber);
+        secondNumber = checkIfNumber(sc);
+        System.out.println("the first number is " + secondNumber);
+
+        System.out.println("The total of the 2 given numbers is " + (firstNumber + secondNumber));
+    }
+
+    public void caseTwo() {
+        Scanner sc = getScanner();
+        char c;
+        String inString;
+
+        System.out.print("Write the word you wish to go through: ");
+        inString = sc.nextLine();
+        System.out.print("Write the letter you want to search for: ");
+        c = sc.nextLine().charAt(0);
+
+        int numOfOccurences = 0;
+        for (int i = 0; i < inString.length(); i++) {
+            char comparison = inString.charAt(i);
+            if (comparison == c) {
+                numOfOccurences++;
+                ;
+            }
         }
+        System.out.println("There's " + numOfOccurences + " " + c + " in the word " + inString);
+    }
+
+    public void caseThree(){
+        
+    }
+
+    // https://stackoverflow.com/questions/71872221/java-how-to-use-a-scanner-to-check-that-input-is-an-integer-and-is-within-a-spe
+    public int checkIfNumber(Scanner sc) {
+        while (!sc.hasNextInt()) {
+            System.out.println("Invalid input, please write a number");
+            sc.nextLine();
+        }
+        return sc.nextInt();
+    }
+
+    public int inputAction() {
+        Scanner sc = getScanner();
+
+        int n = 0;
+        while (n < 1 || n > 5) {
+            n = checkIfNumber(sc);
+        }
+        return n;
+    }
 }
