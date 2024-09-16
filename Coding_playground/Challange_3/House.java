e30dbea1b72d1a7f28d7d881d672f269fc552654
Tan-Thai <Vi.Tan.Thai@gmail.com>
2024-09-16 14:27:37 +0200

    Starting on Challange 3

---
+++ Coding_playground/Challange_3/House.java
@@ -0,0 +1,17 @@
+package Coding_playground.Challange_3;
+
+public class House {
+    double squareMeters;
+    int floors;
+    double value;
+    int buildYear;
+    String address;
+
+    public House() {
+        squareMeters = 120;
+        floors = 0;
+        value = 0;
+        buildYear = 1990;
+        address = "Address name not yet implemented";
+    }
+}
